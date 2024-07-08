368a45fb8b957bd5b1c9d07e3358f1ba8182c7b3
shuail0 <11707571@qq.com>
2024-07-08 14:29:50 +0800

    增加jupiterASR空投查询

--- src/jupiterASR.js
+++ src/jupiterASR.js
@@ -20,38 +20,44 @@ const headers = {
 const request = axios.create({ headers, PROXY, timeout: 10000 });
 
 const tokenSymbolInfo = {
-    'WENWENvqqNya429ubCdR81ZmD69brwQaaBYY6p3LCpk':{ symbol : 'WEN'},
-    'ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq':{ symbol : 'ZEUS'},
-    'SHARKSYJjqaNyxVfrpnBN9pjgkhwDhatnMyicWPnr1s':{ symbol : 'SHARK'},
-    'UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG':{ symbol : 'UPT'},
-    'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN':{ symbol : 'JUP'},
+    'WENWENvqqNya429ubCdR81ZmD69brwQaaBYY6p3LCpk': { symbol: 'WEN' },
+    'ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq': { symbol: 'ZEUS' },
+    'SHARKSYJjqaNyxVfrpnBN9pjgkhwDhatnMyicWPnr1s': { symbol: 'SHARK' },
+    'UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG': { symbol: 'UPT' },
+    'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN': { symbol: 'JUP' },
 }
 
+const connection = new Connection('https://api.mainnet-beta.solana.com', 'confirmed');
+
+for (const key in tokenSymbolInfo) {
+    const mintPublicKey = new PublicKey(key);
+    const mintInfo = await getMint(connection, mintPublicKey);
+    tokenSymbolInfo[key] = {...tokenSymbolInfo[key], ...mintInfo};
+
+}
 
 async function queryJupterASRAirdrop(walletAddress) {
+
     const url = `https://worker.jup.ag/jup-asr-july-2024-claim-proof/${walletAddress}`;
-    const connection = new Connection('https://api.mainnet-beta.solana.com', 'confirmed');
 
     try {
         const res = await request.get(url);
         const data = res.data;
-        if (typeof res.data === 'object' && res.data.claim.length > 0) { 
+        if (typeof res.data === 'object' && res.data.claim.length > 0) {
 
             const amountInfo = {};
             for (const item of data.claim) {
                 const mint = item.mint;
-                const mintPublicKey = new PublicKey(mint);
-                const mintInfo = await getMint(connection, mintPublicKey);
-                const amount = item.amount / Math.pow(10, mintInfo.decimals);
+                const amount = item.amount / Math.pow(10, tokenSymbolInfo[mint]['decimals']);
                 amountInfo[tokenSymbolInfo[mint].symbol] = amount;
             }
-            return { state: 'sucess', address: walletAddress,  voteCount: data.voteCount, ...amountInfo};
+            return { state: 'sucess', address: walletAddress, voteCount: data.voteCount, ...amountInfo };
 
         } else {
             return { state: 'sucess', address: walletAddress, voteCount: data.voteCoun };
         }
     } catch (error) {
-        return { state: 'error', address: walletAddress};
+        return { state: 'error', address: walletAddress };
     }
 };
 

6de94ff22a54d611c5f83b118767839aca7f1798
shuail0 <11707571@qq.com>
2024-07-08 13:57:08 +0800

    增加jupiterASR空投查询

--- config.json.js
+++ config.json.js
@@ -11,24 +11,28 @@ export const OUTPUTPATH = DATAPATH + '/output'; // OUTPUT目录
 // --------最大重试次数--------
 export const MAXRETRY = 3;
 
+
+// ----------------RPC配置----------------
+export const SOLRPC = 'https://api.mainnet-beta.solana.com'; // SOL RPC地址
+
 // --------代理配置,不使用代理这里留空--------
 export const PROXY = {
-    host: '',
+    host: '85.239.53.78',
     port: 33000,
     auth: {
-        username: '',
-        password: ''
+        username: 'wrtest66781-sess_8zix1690gxdp_1+hk',
+        password: 'AsdBBcDtRERy'
     }
 };
 
 // ----------------空投查询配置----------------
 
 // --------Jupiter空投查询配置--------
-export const JUPWALLETPATH = '/Users/lishuai/Documents/crypto/bockchainbot/SOLTestWalle.csv'; // Jup空投钱包路径
+export const JUPWALLETPATH = '/Users/lishuai/Documents/crypto/bockchainbot/SOLTestWalle加密.csv'; // Jup空投钱包路径
 export const JUPTOKENMINT = 'UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG'; // JUP要查询的token合约地址,在空投查询页面可以找到
 
 // --------ZKSYNC空投查询配置--------
 export const ZKWALLETPATH = '/Users/lishuai/Downloads/脚本钱包地址.csv'; // Jup空投钱包路径
 
 // --------LAYZERO空投查询配置--------
-export const LAYZEROWALLETPATH = '/Users/lishuai/Downloads/脚本钱包地址.csv'; // Jup空投钱包路径
+export const LAYZEROWALLETPATH = '/Volumes/s1.s100.vip/小号专用/撸毛/帅总300L0.csv'; // 空投钱包路径
---
+++ src/jupiterASR.js
@@ -0,0 +1,100 @@
+import axios from 'axios-https-proxy-fix';
+import fs from 'fs';
+import csv from 'csv-parser';
+import * as csvStringify from 'csv-stringify';
+import { PROXY, MAXRETRY, JUPWALLETPATH, JUPTOKENMINT, OUTPUTPATH, SOLRPC } from '../config.json.js';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { fileURLToPath } from 'url';
+import { parse } from 'path';
+import Logger from "@youpaichris/logger";
+import { randomInt } from 'crypto';
+import { getMint, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+const logger = new Logger();
+
+const __filename = fileURLToPath(import.meta.url);
+const parsedPath = parse(__filename);
+const filename = parsedPath.name;
+const headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+}
+const request = axios.create({ headers, PROXY, timeout: 10000 });
+
+const tokenSymbolInfo = {
+    'WENWENvqqNya429ubCdR81ZmD69brwQaaBYY6p3LCpk':{ symbol : 'WEN'},
+    'ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq':{ symbol : 'ZEUS'},
+    'SHARKSYJjqaNyxVfrpnBN9pjgkhwDhatnMyicWPnr1s':{ symbol : 'SHARK'},
+    'UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG':{ symbol : 'UPT'},
+    'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN':{ symbol : 'JUP'},
+}
+
+
+async function queryJupterASRAirdrop(walletAddress) {
+    const url = `https://worker.jup.ag/jup-asr-july-2024-claim-proof/${walletAddress}`;
+    const connection = new Connection('https://api.mainnet-beta.solana.com', 'confirmed');
+
+    try {
+        const res = await request.get(url);
+        const data = res.data;
+        if (typeof res.data === 'object' && res.data.claim.length > 0) { 
+
+            const amountInfo = {};
+            for (const item of data.claim) {
+                const mint = item.mint;
+                const mintPublicKey = new PublicKey(mint);
+                const mintInfo = await getMint(connection, mintPublicKey);
+                const amount = item.amount / Math.pow(10, mintInfo.decimals);
+                amountInfo[tokenSymbolInfo[mint].symbol] = amount;
+            }
+            return { state: 'sucess', address: walletAddress,  voteCount: data.voteCount, ...amountInfo};
+
+        } else {
+            return { state: 'sucess', address: walletAddress, voteCount: data.voteCoun };
+        }
+    } catch (error) {
+        return { state: 'error', address: walletAddress};
+    }
+};
+
+
+
+
+; (async () => {
+    let airDropDatas = [];
+    const dataProcessingPromises = [];
+
+    fs.createReadStream(JUPWALLETPATH)
+        .pipe(csv())
+        .on('data', (row) => {
+            dataProcessingPromises.push((async () => {
+                const walletAddress = row['Address'];
+                const walletName = row['Wallet'];
+                let retry = 0;
+                let res = null;
+                let airDropData = {};
+                while (retry < MAXRETRY) {
+                    await new Promise(resolve => setTimeout(resolve, randomInt(1000, 5000)));
+                    res = await queryJupterASRAirdrop(walletAddress);
+                    if (res.state === 'sucess') {
+                        airDropData = { ...res };
+                        break;
+                    }
+                    retry++;
+                }
+                if (retry === MAXRETRY) {
+                    airDropData = { walletName, walletAddress };
+                }
+                logger.success(airDropData);
+                airDropDatas.push(airDropData);
+            })());
+        })
+        .on('end', async () => {
+            await Promise.all(dataProcessingPromises);
+            csvStringify.stringify(airDropDatas, { header: true }, (err, output) => {
+                if (err) throw err;
+                fs.writeFile(`${OUTPUTPATH}/${filename}QueryData.csv`, output, (err) => {
+                    if (err) throw err;
+                    logger.info(`查询完毕，结果保存在: data/output/${filename}QueryData.csv`);
+                });
+            });
+        });
+})();
